0bbc39915bb7544e988dfea3639883ad4372bd98
Leandro Hugo <48139768+LeandroFreitass@users.noreply.github.com>
2021-12-08 19:34:54 -0300

    Atualizacão Supervisora dia 08-12-2021

--- Classes Comuns/ComunicadoDeVez.java
+++ Classes Comuns/ComunicadoDeVez.java
@@ -2,11 +2,14 @@
 public class ComunicadoDeVez extends Comunicado {
 	private int posicaoJogador;
 	
-	public ComunicadoDeVez () {
+	public ComunicadoDeVez (ComunicadoDeDados dados) {
 		this.posicaoJogador = posicaoJogador;
+		this.dados = dados;
 	}
 	public int getposicaodoJogador() {
 		return this.posicaoJogador;
 	}
+	
+	public ComunicadoDeDados dados; 
 	
 }
